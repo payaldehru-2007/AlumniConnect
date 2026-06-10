502308293ce6c750fdabdcbd1fb23310aab66548
payaldehru-2007 <payaldehru@gmail.com>
2026-06-10 16:56:21 +0530

    Final Vercel backend fix

--- alumni-backend/index.js
+++ alumni-backend/index.js
@@ -1,1 +1,20 @@
-module.exports = require('./server');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const alumniRoutes = require('./routes/alumni');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/alumni', alumniRoutes);
+app.use('/auth', authRoutes);
+
+app.get('/', (req, res) => {
+  res.send('AlumniConnect Backend is Running!');
+});
+
+module.exports = app;
