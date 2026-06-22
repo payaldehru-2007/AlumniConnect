4f828cb87040410fa1b436d9b09d40f4531030d8
payaldehru-2007 <payaldehru@gmail.com>
2026-06-22 12:04:33 +0530

    Restore missing admin students route

--- alumni-frontend/src/App.js
+++ alumni-frontend/src/App.js
@@ -24,7 +24,7 @@ function App() {
         <Route path="/admin" element={<Dashboard />} />
         <Route path="/admin/alumni" element={<ManageAlumni />} />
         <Route path="/admin/alumni/edit/:id" element={<EditAlumni />} />
-        <Route path="/admin/alumni/profile/:id" element={<AlumniProfile />} />
+       <Route path="/admin/students" element={<ManageStudents />} />
        <Route path="/admin/search" element={<DeepSearch />} />
 
         {/* Alumni Routes */}
