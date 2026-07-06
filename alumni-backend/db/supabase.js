e7d63a09329421060e71345d874ef1ebab85615e
payaldehru-2007 <payaldehru@gmail.com>
2026-07-06 14:20:06 +0530

    Update Supabase credentials to new project

--- alumni-backend/db/supabase.js
+++ alumni-backend/db/supabase.js
@@ -1,9 +1,7 @@
 const { createClient } = require('@supabase/supabase-js');
 require('dotenv').config();
-
 const supabase = createClient(
-  'https://uzgzetsuxbwwibcklpea.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6Z3pldHN1eGJ3d2liY2tscGVhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MDAyMzU4OSwiZXhwIjoyMDk1NTk5NTg5fQ.XOqVUGBV1T9Mp6MsUsVqy8PpoMqldv3XQsFoES5aFEI'
+  'https://pkjidofzjzmluoglporl.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBramlkb2Z6anptbHVvZ2xwb3JsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MzMyMTU5MiwiZXhwIjoyMDk4ODk3NTkyfQ.drGqYbzI6J2NXLB8n4zNceDwfg9bcr8hCJ_Ua-Yu5gs'
 );
-
 module.exports = supabase;
